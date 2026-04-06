ef0fa43527f0c0549864c1157e86f5d731221262
joseoliveiraantunes-hash <jose.oliveira.antunes@escola.pr.gov.br>
2026-04-06 11:04:56 -0300

    alteracao

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-document.addEventListener("DOMContentLoaded", function())
-//Corrige o ul da agenda caso a classe nao esteja certa
-const secaoAgenda= document.querySelector("agenda");
+document.addEventListener("DOMContentLoad", function()){
+    const secaoAgenda = document.querySelector("#agenda");
+    if (secaoAgenda) {
+        counst ulAgenda.clasList.add("agenda");
+    }
+{
+    cost secaoContato = document.querySelector("#contato");
+    if(secaoContato){
+        secaoContato.classicList.add(contato-box);
+    }
 
+    //seleciona links do menu
+    const linksMenu = document.querySelector("nav a");
+    const secoes = document.querySelectorAll("section[id]");
 
-{);
+    //Deixa a transição suave nos cards via js
+    const cards = document.querySelectionAll("card");
+    for(let i= 0; i < cards.length; i++{
+        cards[i].style.transition = "0.3s ease", function(){
+            this.style.tarnsform = "translateY(-6px)";
+        });
+
+         cards[i].style.transition = "0.3s ease", function(){
+            this.style.tarnsform = "translateY(-6px)";
+           
+         });  
